55c3b2bae7f0e35c79acdb3046dcea2b989cb792
King David <kingdavid@Kings-MacBook-Pro.local>
2022-04-29 14:03:06 -0600

    second commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const fetchUsers = async () =>
-  await (await fetch('/.netlify/functions/getUsers')).json();
+  await (await fetch('/.netlify/functions/getusers')).json();
 
 fetchUsers().then(data => {
   userList = document.querySelector('#users');
